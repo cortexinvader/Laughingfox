7a452d441290322381cbd417c1defda0eef6bcb6
Lance <149887891+lance-ui@users.noreply.github.com>
2025-11-11 00:46:57 -0800

    Update handleDatabase.js

--- handler/handleDatabase.js
+++ handler/handleDatabase.js
@@ -6,15 +6,17 @@ const prefixesDataMap = new Map();
 const groupDataMap = new Map();
 
 const initCache = async () => {
-  const userMoneyData = await db.getTable('userMoney');
-  const userData = await db.getTable('userData');
-  const prefixesData = await db.getTable('prefixesData');
-  const groupData = await db.getTable('groupData');
+  const userMoneyData = await db.getTable("userMoney");
+  const userData = await db.getTable("userData");
+  const prefixesData = await db.getTable("prefixesData");
+  const groupData = await db.getTable("groupData");
 
   userMoneyData.forEach((user) => userMoneyMap.set(user.id, user));
   userData.forEach((user) => userDataMap.set(user.id, user));
   prefixesData.forEach((prefix) => prefixesDataMap.set(prefix.id, prefix));
-  groupData.forEach((group) => groupDataMap.set(`${group.id}-${group.uid}`, group));
+  groupData.forEach((group) =>
+    groupDataMap.set(`${group.id}-${group.uid}`, group),
+  );
 };
 
 initCache();
@@ -23,18 +25,18 @@ const OMIT = 2 * 60 * 1000; // 2 minutes
 
 const saveData = async () => {
   try {
-    await saveTable('userMoney', Array.from(userMoneyMap.values()));
-    await saveTable('userData', Array.from(userDataMap.values()));
-    await saveTable('prefixesData', Array.from(prefixesDataMap.values()));
+    await saveTable("userMoney", Array.from(userMoneyMap.values()));
+    await saveTable("userData", Array.from(userDataMap.values()));
+    await saveTable("prefixesData", Array.from(prefixesDataMap.values()));
     await saveTable(
-      'groupData',
+      "groupData",
       Array.from(groupDataMap.values()).map((group) => ({
         id: group.id,
         name: group.name,
         uid: group.uid,
         banned: group.banned,
         msgCount: group.msgCount || 0,
-      }))
+      })),
     );
   } catch (error) {
     console.error("Error saving data:", error);
@@ -50,33 +52,44 @@ const shutdown = async () => {
   process.exit(0);
 };
 
-process.on('SIGINT', shutdown);
-process.on('SIGTERM', shutdown);
-process.on('SIGUSR1', shutdown);
-process.on('SIGUSR2', shutdown);
-process.on('exit', () => {});
+process.on("SIGINT", shutdown);
+process.on("SIGTERM", shutdown);
+process.on("SIGUSR1", shutdown);
+process.on("SIGUSR2", shutdown);
+process.on("exit", () => {});
 
-const handleDatabase = async ({ threadID, senderID, sock, event }) => {
+const handleDatabase = async ({ threadID, senderID, sock }) => {
   try {
     if (!userMoneyMap.has(senderID)) {
       userMoneyMap.set(senderID, { id: senderID, money: 0, msgCount: 0 });
     }
 
     if (!userDataMap.has(senderID)) {
-      userDataMap.set(senderID, { id: senderID, name: event.pushName || "unknown" });
+      userDataMap.set(senderID, { id: senderID, name: "Unknown" });
     }
 
     if (!prefixesDataMap.has(threadID)) {
-      prefixesDataMap.set(threadID, { id: threadID, prefix: global.client.config.PREFIX });
+      prefixesDataMap.set(threadID, {
+        id: threadID,
+        prefix: global.client.config.PREFIX,
+      });
     }
-    
-    if (!groupDataMap.has(`${threadID}-${senderID}`) && threadID.endsWith("@g.us")) {
+
+    if (
+      !groupDataMap.has(`${threadID}-${senderID}`) &&
+      threadID.endsWith("@g.us")
+    ) {
       const groupMetadata = async () => {
         const groupInfo = await sock.groupMetadata(threadID);
         return groupInfo ? groupInfo.subject : "Unknown Group";
       };
       const groupName = await groupMetadata();
-      groupDataMap.set(`${threadID}-${senderID}`, { id: threadID, name: groupName, uid: senderID, banned: 0 });
+      groupDataMap.set(`${threadID}-${senderID}`, {
+        id: threadID,
+        name: groupName,
+        uid: senderID,
+        banned: 0,
+      });
     }
 
     const userMoney = userMoneyMap.get(senderID);
@@ -100,7 +113,7 @@ const setuserBanned = async (userId, banned) => {
     const userData = userDataMap.get(userId);
     userData.banned = banned ? 1 : 0;
     userDataMap.set(userId, userData);
-    await saveTable('userData', Array.from(userDataMap.values()));
+    await saveTable("userData", Array.from(userDataMap.values()));
   }
 };
 
@@ -110,7 +123,24 @@ const setgroupBanned = async (groupId, banned) => {
   if (groupData) {
     groupData.banned = banned ? 1 : 0;
     groupDataMap.set(`${groupData.id}-${groupData.uid}`, groupData);
-    await saveTable('groupData', Array.from(groupDataMap.values()).map((group) => ({ id: group.id, name: group.name, uid: group.uid, banned: group.banned })));
+    await saveTable(
+      "groupData",
+      Array.from(groupDataMap.values()).map((group) => ({
+        id: group.id,
+        name: group.name,
+        uid: group.uid,
+        banned: group.banned,
+      })),
+    );
   }
 };
-export { setgroupBanned, setuserBanned, handleDatabase}
+
+const dataCache = {
+  userMoney: userMoneyMap,
+  userData: userDataMap,
+  prefixesData: prefixesDataMap,
+  groupData: groupDataMap,
+};
+
+export { setgroupBanned, setuserBanned, handleDatabase, dataCache };
+export default dataCache;
