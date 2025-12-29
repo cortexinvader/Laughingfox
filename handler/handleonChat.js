748990f08260b3b5d8898a7d17dd2bd17441f017
Lance <149887891+lance-ui@users.noreply.github.com>
2025-12-29 16:22:57 +0100

    Update handleonChat.js

--- handler/handleonChat.js
+++ handler/handleonChat.js
@@ -1,51 +1,47 @@
 export default async ({
-            sock,
-            event,
-            threadID,
-            senderID,
-            proto,
-            font,
-            bot,
-            message,
-            args,
-            dataCache,
-            saveTable,
-            getPrefixesData,
-            getTable,
-            getUserData,
-            getGroupData,
-            getUserMoney,
-            setuserBanned,
-            setgroupBanned
-   }) => {
-     try{
-     const cmdsArray = Array.from(global.client.commands.values())
-     for(const cmd of cmdsArray){
-       if(cmd?.onChat){
-         await cmd.onChat({
-            sock,
-            event,
-            threadID,
-            senderID,
-            proto,
-            font,
-            bot,
-            message,
-            args,
-            dataCache,
-            saveTable,
-            getPrefixesData,
-            getTable,
-            getUserData,
-            getGroupData,
-            getUserMoney,
-            setuserBanned,
-            setgroupBanned
-         })
-       }
-     }
-     } catch (error){
-       message.send("Failed to handle onChat check yout console to know the error")
-       console.log(error)
-     }
-   }
+  sock,
+  event,
+  threadID,
+  senderID,
+  proto,
+  font,
+  bot,
+  message,
+  args,
+  dataCache,
+  saveTable,
+  getTable,
+  getUserData,
+  getGroupData,
+  setuserBanned,
+  setgroupBanned
+}) => {
+  try {
+    const cmdsArray = Array.from(global.client.commands.values());
+    for (const cmd of cmdsArray) {
+      if (cmd?.onChat) {
+        await cmd.onChat({
+          sock,
+          event,
+          threadID,
+          senderID,
+          proto,
+          font,
+          bot,
+          message,
+          args,
+          dataCache,
+          saveTable,
+          getTable,
+          getUserData,
+          getGroupData,
+          setuserBanned,
+          setgroupBanned
+        });
+      }
+    }
+  } catch (error) {
+    message.send("Failed to handle onChat check your console to know the error");
+    console.log(error);
+  }
+};
