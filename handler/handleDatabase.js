be2a9a7042ed8b8a7054030aa9257ed97be763c4
Lance <149887891+lance-ui@users.noreply.github.com>
2025-12-29 16:18:37 +0100

    Update handleDatabase.js

--- handler/handleDatabase.js
+++ handler/handleDatabase.js
@@ -1,70 +1,49 @@
 import db, {
   saveTable,
-  getUserMoney,
   getUserData,
-  getPrefixesData,
   setUserBanned,
   setGroupBanned,
 } from "../utils/data.js";
 
-const updateUserData = async (data, table) => {
-  await saveTable(table, [data]);
+const updateUserData = async (data) => {
+  await saveTable("userData", [data]);
 };
 
 const updateGroupData = async (data) => {
-    await saveTable("groupData", [data]);
-}
+  await saveTable("groupData", [data]);
+};
 
 const handleDatabase = async ({ threadID, senderID, sock, event }) => {
   try {
-    let userMoney = await getUserMoney(senderID);
-    if (!userMoney.id) {
-      userMoney = { id: senderID, money: 0, msgCount: 0 };
-    }
-    userMoney.msgCount = (userMoney.msgCount || 0) + 1;
-    await updateUserData(userMoney, "userMoney");
-
     let userData = await getUserData(senderID);
-    if (!userData.id) {
-      userData = { id: senderID, name: event.pushName, banned: 0, exp: 0, data: {} };
-      await updateUserData(userData, "userData");
-    }
+    
+    userData.name = userData.name || event.pushName || "Unknown";
+    userData.msgCount = (userData.msgCount || 0) + 1;
+    userData.money = (userData.money || 0) + 10; 
 
-    const prefixesData = await db.getTable("prefixesData");
-    let prefixEntry = prefixesData.find((p) => p.id === threadID);
-
-    if (!prefixEntry) {
-      prefixEntry = {
-        id: threadID,
-        prefix: global.client.config.PREFIX,
-      };
-      await updateUserData(prefixEntry, "prefixesData");
-    }
+    await updateUserData(userData);
 
     if (threadID.endsWith("@g.us")) {
       const groupDataArray = await db.getTable("groupData");
-      let groupData = groupDataArray.find(
-        (group) => group.id === threadID);
-      
+      let groupData = groupDataArray.find((group) => group.id === threadID);
+
       if (!groupData) {
-        const groupMetadata = async () => {
-          const groupInfo = await sock.groupMetadata(threadID);
-          return groupInfo ? groupInfo.subject : "Unknown Group";
-        };
-        const groupName = await groupMetadata();
+        const groupInfo = await sock.groupMetadata(threadID);
+        const groupName = groupInfo ? groupInfo.subject : "Unknown Group";
 
         groupData = {
           id: threadID,
           name: groupName,
           banned: 0,
+          msgCount: 0
         };
       }
-      
+
       groupData.msgCount = (groupData.msgCount || 0) + 1;
       await updateGroupData(groupData);
     }
   } catch (e) {
-    console.log(`Error in handleDatabase: ${e.message}`);
+    console.error(`Error in handleDatabase: ${e.message}`);
   }
 };
 
@@ -76,8 +55,7 @@ const setgroupBannedDirect = async (groupId, banned) => {
   await setGroupBanned(groupId, banned);
 };
 
-const dataCache = {
-};
+const dataCache = {};
 
 export { 
     setgroupBannedDirect as setgroupBanned, 
